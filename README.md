53a7fce94d49588a168c3f003925f51e61b3bcdd
hendrikmitk <hharlichs@googlemail.com>
2021-03-10 15:57:50 +0100

    Set up service and first endpoints

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# romey-api
-Travel Blog API ⚙️
+# Project: Travel Blog API ⚙️
+
+Simple Node.js application that powers the backend for [romey](https://github.com/hendrikmitk/romey) and serves blog post data via a REST API in JSON format.
+
+This application is an optional part of the final project [Travel Blog in Vue.js 🌍](https://github.com/hendrikmitk/romey), which is part of the curriculum of the Full-Stack Web Development program at [Hamburg Coding School](https://hamburgcodingschool.com/).
---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+/////////////////
+// I M P O R T //
+/////////////////
+
+const spotData = require('./utils/spotData');
+
+/////////////////
+// R O U T E S //
+/////////////////
+
+// GET ALL SPOTS
+app.get('/spots', (req, res) => {
+	res.json({
+		code: 200,
+		status: 'OK',
+		description: 'Selected all spot data from database',
+		data: spotData
+	});
+});
+
+// GET ONE SPOT BY ID
+app.get('/spots/:id', (req, res) => {
+	const paramId = req.params.id;
+	const spotIds = []; // Array to store all spotIds in
+
+	// Iterate trough spotData
+	spotData.forEach(spot => {
+		spotIds.push(spot.id.toString()); // Convert ids to strings and store in array
+
+		// Return spot object when id found
+		if (spot.id == paramId) {
+			res.json({
+				code: 200,
+				status: 'OK',
+				description: 'Selected one spot from database',
+				data: spot
+			});
+		}
+	});
+
+	// Check if requested spotId exist in spotIds
+	if (!spotIds.includes(paramId)) {
+		res.json({
+			code: 404,
+			status: 'Not Found', // spotId does not exist
+			description: 'Spot does not exist in database',
+			data: {}
+		});
+	}
+});
+
+/////////////////
+// L I S T E N //
+/////////////////
+app.listen(port, () => {
+	console.log(`Server running and listening on port ${port}`);
+});
