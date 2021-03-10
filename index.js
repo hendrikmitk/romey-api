1e5f5917cea5a50ab0c0c059bbeab0936b8d508b
hendrikmitk <hharlichs@googlemail.com>
2021-03-10 18:45:57 +0100

    Set Access-Control-Allow-Origin header

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
+
 app.use(express.json());
 
 /////////////////
